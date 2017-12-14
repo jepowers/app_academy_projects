d8489ad1dce44472bf8ab19610646613d15bfa64
Joseph Powers <jepowers@gmail.com>
2017-12-13 18:12:26 -0800

    Work for week 2

--- other_assignments/W2D2/Chess/piece.rb
+++ other_assignments/W2D2/Chess/piece.rb
@@ -1,8 +1,9 @@
-# require_relative 'board'
-class Piece
-  # attr_accessor :name
+require_relative 'board'
 
-  def initialize(color)(color)
+class Piece
+  attr_accessor :name
+
+  def initialize(color)
     @chess_piece = "\u265e"
     @color = color
   end
@@ -28,38 +29,38 @@ end
 
 class Queen < Piece
   def initialize(color)
-    @chess_piece = "\u2655"
+    color == :white ? @chess_piece = "\u2655" : @chess_piece = "\u265B"
   end
 end
 
 class King < Piece
   def initialize(color)
-    @chess_piece = "\u2654"
+    color == :white ? @chess_piece = "\u2654" : @chess_piece = "\u265A"
   end
 end
 
 class Bishop < Piece
   def initialize(color)
     # super(color)
-    @chess_piece = "\u2657"
+    color == :white ? @chess_piece = "\u2657" : @chess_piece = "\u265D"
   end
 end
 
 class Rook < Piece
   def initialize(color)
-    @chess_piece = "\u2656"
+    color == :white ? @chess_piece = "\u2656" : @chess_piece = "\u265C"
   end
 end
 
 class Knight < Piece
   def initialize(color)
-    @chess_piece = "\u2658"
+    color == :white ? @chess_piece = "\u2658" : @chess_piece = "\u265E"
   end
 end
 
 class Pawn < Piece
   def initialize(color)
-    @chess_piece = "\u2659"
+    color == :white ? @chess_piece = "\u2659" : @chess_piece = "\u265F"
   end
 end
 
